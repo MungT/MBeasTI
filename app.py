99f4d86b0467486e4df522924925cdd67b2c5c08
Dundun2 <skyp2009@naver.com>
2022-07-13 11:11:52 +0900

    app 추가

--- app.py
+++ app.py
@@ -166,6 +166,35 @@ def getComment():
     all_comment = list(db.comment.find({},{'_id':False}))
     return jsonify({'msg': all_comment})
 
+#---------------------------------------------------------------------------------------
+# MBTI 검사 관련
+
+@app.route('/index')
+def index():
+
+    return render_template("index.html")
+
+@app.route('/index2')
+def index2():
+
+    return render_template("index2.html")
+
+@app.route('/index3')
+def index3():
+
+    return render_template("index3.html")
+
+@app.route('/index4')
+def index4():
+
+    return render_template("index4.html")
+
+@app.route('/index5')
+def index5():
+
+    return render_template("index5.html")
+
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
